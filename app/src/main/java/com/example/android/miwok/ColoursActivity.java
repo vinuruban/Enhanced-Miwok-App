2a72b092435cd12b24e5a7015f410fa7b20ba54e
Vinukshen <vinukshen@gmail.com>
2020-04-18 13:39:08 +0100

    visual polish + different BGColours added to activities

--- app/src/main/java/com/example/android/miwok/ColoursActivity.java
+++ app/src/main/java/com/example/android/miwok/ColoursActivity.java
@@ -39,7 +39,7 @@ public class ColoursActivity extends AppCompatActivity {
         words.add(new Word("ṭopiisә", "dusty yellow", R.drawable.color_dusty_yellow));
         words.add(new Word("chiwiiṭә", "mustard yellow", R.drawable.color_mustard_yellow));
 
-        WordAdapter adapter = new WordAdapter(this, words);
+        WordAdapter adapter = new WordAdapter(this, words, R.color.category_colors);
         ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(adapter);
     }
--- app/src/main/java/com/example/android/miwok/PhrasesActivity.java
+++ app/src/main/java/com/example/android/miwok/PhrasesActivity.java
@@ -41,7 +41,7 @@ public class PhrasesActivity extends AppCompatActivity {
         words.add(new Word("yoowutis", "Let’s go."));
         words.add(new Word("әnni'nem", "Come here."));
 
-        WordAdapter adapter = new WordAdapter(this, words);
+        WordAdapter adapter = new WordAdapter(this, words, R.color.category_phrases);
         ListView listView = (ListView) findViewById(R.id.list);
         listView.setAdapter(adapter);
     }
